dea4b8e4ed7d5237519ef65e09fc46027c1df9cc
King Odo <Ogblowoodo@gmail.com>
2025-06-15 10:48:50 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './index.css';
+import './App.css'
+import Email from './Email';
+import Box from '@mui/material/Box';
+import { Icon, Typography } from '@mui/material';
+import FacebookSharpIcon from '@mui/icons-material/FacebookSharp';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+
+
+function App() {
+  return (
+   <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+   
+    <img src="logo.svg" alt="logo" width={90} height={29} style={{margin:"1.5rem 0"}}/>
+    <h1><span style={{color: '#969696', fontWeight:"400"}}>We are launching </span>soon!</h1>
+    <p style={{fontWeight:"300"}}>Subscribe and get notified</p>
+      
+    <Email/>
+    <img src="illustration-dashboard.png" alt="dashboard-Illustration"  width={640} height={380} />
+    
+    
+    <div className="icons">
+      {/* this section contains icons such as IG, FB, Twitter, etc. */}
+      <Icon sx={{color:"#4C7BF3", border:"1px solid #B8C7ED", borderRadius:"4.5rem", margin:"1rem"}}>
+      <FacebookSharpIcon></FacebookSharpIcon>
+      </Icon>
+      <Icon sx={{color:"#4C7BF3", border:"1px solid #B8C7ED", borderRadius:"4.5rem", margin:"1rem"}}>
+         <TwitterIcon></TwitterIcon>
+      </Icon>
+      <Icon sx={{color:"#4C7BF3", border:"1px solid #B8C7ED", borderRadius:"4.5rem", margin:"1rem", ":hover": {backgroundColor:"hsl(223, 87%, 63%)s"}}}>
+         <InstagramIcon></InstagramIcon>
+      </Icon>
+  
+    </div>
+    <Typography sx={{color:"#969696", fontWeight:"300", marginTop:"0.5rem"}}>&copy; Copyright Ping. All rights reserved</Typography>
+   
+   </Box>
+  )
+}
+
+export default App
---
+++ src/Email.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Box, Button, Input, Typography} from '@mui/material'
+import  { useState } from "react";
+
+const Email = () => {
+  const [isValid, setisValid]= useState(true);
+ const isEmail = (email: string) => {
+   // Simple and valid email regex. printed from aI
+   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+ };
+
+const validate =(email:string) => {
+  const isValidEmail= isEmail(email) 
+  if(isValidEmail){
+    setisValid(true);
+  } else{
+    setisValid(false);
+  }
+};
+
+  return (
+    <Box sx={{marginBottom:"4rem"}}>
+      <Input sx={{border: `1px solid ${isValid ? "#B8C7ED" : "#FF5466" }`, borderRadius:"2rem", color:"#B8C7ED",
+      width:"26.3125rem", padding:"0.5rem 1rem", 
+      marginRight:"1rem",
+      }} 
+      onChange={(e) => {validate(e.target.value)}}
+      placeholder='Your email address...' />
+
+      {isValid ? null:(
+        <Typography sx={{color:"#FF5466"}}>Please provide a valid email address</Typography>)
+      }
+      <Button className='notify-btn' sx={{backgroundColor:"#4C7BF3", color:"#FFF", borderRadius:"2rem", padding:"0.5rem 1rem", width:"200px", height:"3.5rem" }}>Notify Me</Button>
+    </Box>
+
+  )
+}
+
+export default Email
